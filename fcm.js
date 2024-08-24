ec078b52a11d009ef68c600ad3764826bc53e52d
Thanapol Pilawal <pilawal.t@gmail.com>
2024-08-24 15:14:54 +0700

    feat(FCM): Migrate from legacy FCM APIs to HTTP v1

--- fcm.js
+++ fcm.js
@@ -1,5 +1,6 @@
 const prompt = require('prompt');
-const FCM = require('fcm-node');
+const { google } = require('googleapis');
+const axios = require('axios');
 
 console.log('--------------------------');
 console.log('Welcome To Push Test FCM');
@@ -8,145 +9,178 @@ console.log('--------------------------');
 prompt.start();
 
 const fcmKeyObj = [
-    {
-      app: 'YOUR-APP-NAME', key: 'YOUR-FCM-KEY',
-    }
+  {
+    app: 'POH4', key: './paradeofhomes4.json',
+  },
 ];
 
-let serverKey = '';
+let serviceAccountPath = '';
 let regisID = '';
+let projectId = '';  // Store the project ID here
 
 console.log('Please input your project name?');
-prompt.get(['ProjectName'], function (err, project) {
-    if (err) { return onErr(err); }
-    console.log('Project: ' + project.ProjectName);
-        for (i = 0; i < fcmKeyObj.length; i++) {
-            if (fcmKeyObj[i].app === project.ProjectName) {
-                serverKey = fcmKeyObj[i].key
-                console.log(`ServerKey is ${serverKey}`);
-            }else{
-                serverKey = fcmKeyObj[0].key
-                console.log('Wrong project name set to default');
-                console.log(`ServerKey is ${serverKey}`);
-            }
+prompt.get(['ProjectName'], async function (err, project) {
+  if (err) { return onErr(err); }
+  console.log('Project: ' + project.ProjectName);
+
+  for (i = 0; i < fcmKeyObj.length; i++) {
+    if (fcmKeyObj[i].app === project.ProjectName) {
+      serviceAccountPath = fcmKeyObj[i].key;
+      console.log(`Service Account Path is ${serviceAccountPath}`);
+    } else {
+      serviceAccountPath = fcmKeyObj[0].key;
+      console.log('Wrong project name set to default');
+      console.log(`Service Account Path is ${serviceAccountPath}`);
+    }
+  }
+
+  const key = require(serviceAccountPath);
+  projectId = key.project_id;
+
+  console.log('Please input registration id android?');
+  prompt.get(['RegisID'], async function (err, data) {
+    if (err) { return onErr('No RegisID'); }
+    regisID = data.RegisID;
+
+    if (regisID === '') {
+      console.log('RegisID: ' + 'blank');
+      onErr('No RegisID');
+    } else {
+      console.log('Use FCM payload template? "type -y for confirm"');
+      prompt.get(['Confirm'], async function (err, template) {
+        if (template.Confirm == '-y') {
+          await sendMessageTemplate(regisID);
+        } else {
+          await sendMessage(regisID);
         }
-        console.log('Please input registration id android?');
-        prompt.get(['RegisID'], function (err, data) {
-            if (err) { return onErr('No RegisID'); }
-            regisID = data.RegisID
-            console.log('Please input descriptions of messange?');
-            if (regisID === '') {
-                console.log('RegisID: ' + 'blank');
-                onErr('No RegisID');
-            } else {
-                console.log('Use fcm payload template? "type -y for confirm"');
-                prompt.get(['Confirm'], function (err, template) {
-                    if(template.Confirm == '-y'){
-                        sendMessageTemplate(regisID);
-                    }else{
-                        sendMessage(regisID);
-                    }
-                });
-            }
-        });
+      });
+    }
+  });
 });
 
-const sendMessageTemplate = (registrationToken) => {
-    const fcm = new FCM(serverKey);
-    const messageTp = {
-        to: registrationToken,
+async function getAccessToken(serviceAccountPath) {
+  const key = require(serviceAccountPath);
+  const jwtClient = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    ['https://www.googleapis.com/auth/firebase.messaging'],
+    null
+  );
+
+  const tokens = await jwtClient.authorize();
+  return tokens.access_token;
+}
+
+async function sendMessageTemplate(registrationToken) {
+  const accessToken = await getAccessToken(serviceAccountPath);
+
+  const message = {
+    message: {
+      token: registrationToken,
+      notification: {
+        title: 'Title of FCM push notification',
+        body: 'Body of FCM push notification',
+      },
+      data: {
+        title: 'Title of FCM push notification',
+        body: 'Body of FCM push notification',
+        userInfo: JSON.stringify({ id: 1, type: 'session' }),
+      },
+    },
+  };
+
+  console.log('Ready to send!');
+  console.log('#####Payload detail#####');
+  console.log(`To: ${message.message.token}`);
+  console.log(`Title: ${message.message.notification.title}`);
+  console.log(`Body: ${message.message.notification.body}`);
+  console.log('Data:');
+  console.log(message.message.data);
+  console.log('########################');
+  console.log('Are you sure to send? "type -y for confirm"');
+
+  prompt.get(['Input'], async function (err, result) {
+    if (err) { return onErr('Message not sent'); }
+    if (result.Input === '-y') {
+      try {
+        const response = await axios.post(
+          `https://fcm.googleapis.com/v1/projects/${projectId}/messages:send`,
+          message,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        console.log('Successfully sent with response:', response.data);
+      } catch (error) {
+        console.log('Something has gone wrong!', error);
+      }
+    } else {
+      onErr('Message will not be sent ^^');
+    }
+  });
+}
+
+async function sendMessage(registrationToken) {
+  const accessToken = await getAccessToken(serviceAccountPath);
+
+  prompt.get(['Title', 'Message', 'Count'], async function (err, messange) {
+    if (err) { return onErr('No Descriptions'); }
+
+    const message = {
+      message: {
+        token: registrationToken,
         notification: {
-            title: 'Title of fcm push notification',
-            body: 'Body of fcm push notification',
+          title: messange.Title,
+          body: messange.Message,
         },
         data: {
-            title: 'Title of fcm push notification',
-            body: 'Body of fcm push notification',
-            msgcnt: 1,
-            style: 'inbox',
-            summaryText: 'There are %n% notifications',
-            ledColor: [0, 51, 255, 255],
-            vibrationPattern: [200, 700, 0, 0],
-            priority: 'high',
-            userInfo: {id: 1, type: "session"},
-        }
+          title: messange.Title,
+          body: messange.Message,
+          msgcnt: messange.Count,
+        },
+      },
     };
+
     console.log('Ready to send!');
     console.log('#####Payload detail#####');
-    console.log(`To: ${messageTp.to}`);
-    console.log(`Title: ${messageTp.notification.title}`);
-    console.log(`Body: ${messageTp.notification.body}`);
+    console.log(`To: ${message.message.token}`);
+    console.log(`Title: ${message.message.notification.title}`);
+    console.log(`Body: ${message.message.notification.body}`);
     console.log('Data:');
-    console.log(messageTp.data);
+    console.log(message.message.data);
     console.log('########################');
     console.log('Are you sure to send? "type -y for confirm"');
-    prompt.get(['Input'], function (err, result) {
-        if (err) { return onErr('Message not send'); }
-        if (result.Input === '-y') {
-            fcm.send(messageTp, function(err, response){
-                if (err) {
-                    console.log('Something has gone wrong!', err);
-                } else {
-                    console.log('Successfully sent with response: ', response);
-                }
-            });
-        } else {
-            onErr('Message will not send ^^');
+
+    prompt.get(['Input'], async function (err, result) {
+      if (err) { return onErr('Message not sent'); }
+      if (result.Input === '-y') {
+        try {
+          const response = await axios.post(
+            `https://fcm.googleapis.com/v1/projects/${projectId}/messages:send`,  // Use the projectId here
+            message,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+          console.log('Successfully sent with response:', response.data);
+        } catch (error) {
+            console.log('Something has gone wrong!', error.response.data); // Add this line
         }
+      } else {
+        onErr('Message will not be sent ^^');
+      }
     });
+  });
 }
 
-const sendMessage = (registrationToken) => {
-    const fcm = new FCM(serverKey);
-    prompt.get(['Title', 'Messange', 'Count'], function (err, messange) {
-        if (err) { return onErr('No Descriptions'); }
-        console.log('Title: ' + messange.Title);
-        console.log('Messange: ' + messange.Messange);
-        console.log('Count: ' + messange.Count);
-        const message = {
-            to: registrationToken,
-            notification: {
-                title: messange.Title,
-                body: messange.Messange,
-                badge: 1
-            },
-            data: {
-                title: messange.Title,
-                body: messange.Messange,
-                msgcnt: messange.Count,
-                style: 'inbox',
-                summaryText: 'There are %n% notifications',
-                ledColor: [0, 51, 255, 255],
-                vibrationPattern: [200, 700, 0, 0],
-            }
-        };
-        console.log('Ready to send!');
-        console.log('#####Payload detail#####');
-        console.log(`To: ${message.to}`);
-        console.log(`Title: ${message.notification.title}`);
-        console.log(`Body: ${message.notification.body}`);
-        console.log('Data:');
-        console.log(message.data);
-        console.log('########################');
-        console.log('Are you sure to send? "type -y for confirm"');
-        prompt.get(['Input'], function (err, result) {
-            if (err) { return onErr('Message not send'); }
-            if (result.Input === '-y') {
-                fcm.send(message, function(err, response){
-                    if (err) {
-                        console.log('Something has gone wrong!', err);
-                    } else {
-                        console.log('Successfully sent with response: ', response);
-                    }
-                });
-            } else {
-                onErr('Message will not send ^^');
-            }
-        });
-    });
+function onErr(err) {
+  console.log(err);
+  return 1;
 }
-
-const onErr = (err) => {
-    console.log(err);
-    return 1;
-};
